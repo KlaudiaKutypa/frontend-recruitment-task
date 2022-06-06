9507933c976e626e3f6324664c9933f8966f7ffa
KlaudiaKutypa <klaudia.kutypa@gmail.com>
2022-06-06 20:38:31 +0200

    additional-task

--- src/js/main.js
+++ src/js/main.js
@@ -35,6 +35,42 @@ close.addEventListener("click", function (event) {
 });;
 
 
+/*ADDITIONAL TASK*/
+const API = "https://jsonplaceholder.typicode.com/users"
 
+const handleSeeProducts = () => {
+    fetch(`${API}`)
+    .then(response => response.json())
+    .then(response => {console.log(response)
+    
+        user = response
+        console.log(user)
+        
+        for ( i=0; i < user.length; i++) {
+
+            console.log(user[i].name)
+            console.log(user[i].email)
+            console.log(user[i].address)
+            console.log(user[i].phone)
+            console.log(user[i].company)
+            
+            const name = document.getElementsByClassName("name");
+            name[i].innerHTML = user[i].name;
+
+            const email = document.getElementsByClassName("email");
+            email[i].innerHTML = user[i].email;
+
+            const address = document.getElementsByClassName("address");
+            address[i].innerHTML = user[i].address.city + "; "+ user[i].address.street + "; "+ user[i].address.suite;
+
+            const phone = document.getElementsByClassName("phone");
+            phone[i].innerHTML = user[i].phone;
+
+            const company = document.getElementsByClassName("company");
+            company[i].innerHTML = user[i].company.name;
+        }
+})}
+
+handleSeeProducts();
 
 
